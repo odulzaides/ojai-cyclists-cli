7b8d8bb7ef049a9aa61a264ea2f5a6385af3c0da
odulzaides <dulzaides.oscar@gmail.com>
2018-06-15 19:26:19 -0400

    TASK: initial sass commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { RulesComponent } from './rules/rules.component';
+import { RidesComponent } from './rides/rides.component';
+import { WeatherComponent } from './weather/weather.component';
+import { PhotosComponent } from './photos/photos.component';
+import { MiscComponent } from './misc/misc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent,
+    NavComponent,
+    RulesComponent,
+    RidesComponent,
+    WeatherComponent,
+    PhotosComponent,
+    MiscComponent
   ],
   imports: [
     BrowserModule
